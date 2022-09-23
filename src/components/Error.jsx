aa325d3f968a595d9e13d7b06632c187790691d7
TsogtOnCrack <tsogt.otgo@gmail.com>
2022-09-23 18:05:46 +0800

    this si error upload

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+export default function Error(){
+    return <div className="flex justify-center items-center h-screen flex-col">
+
+    <h1 className=" font-bebas text-[40px] text-white">SORRY</h1>
+    <h1 className="font-bebas text-[30px] text-red-500">This website is under maintanence at the moment</h1>
+    <p className=" font-roboto text-[18px] text-white/60 md:w-[30vw] w-[80vw] text-center my-5">
+        The application part is being updated. Some of the questions were not clear
+    </p>
+    <h1 className=" font-bebas text-[40px] text-white">Please return in 2 hours</h1>
+
+    </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,56 +6,59 @@ import Image from 'next/image'
 import schoolpic from './schoolpic.jpeg'
 import schoolpic2 from './schoolpic2.jpeg'
 import code from './code.jpeg'
+import Error from '../components/Error'
 
 export default function Page() {
-  const [fix, setFix] = useState(false)
+  // const [fix, setFix] = useState(false)
 
-  if (typeof window !== 'undefined') {
-    const setFixed = () => {
-      if (window.scrollY >= 100) {
-        setFix(true)
-      } else {
-        setFix(false)
-      }
-      // console.log(fix)
-    }
+  // if (typeof window !== 'undefined') {
+  //   const setFixed = () => {
+  //     if (window.scrollY >= 100) {
+  //       setFix(true)
+  //     } else {
+  //       setFix(false)
+  //     }
+  //     // console.log(fix)
+  //   }
 
-    window.addEventListener('scroll', setFixed)
-  }
+  //   window.addEventListener('scroll', setFixed)
+  // }
 
-  return (
-    <div className=" flex flex-col items-center justify-center ">
-      <Header fix={fix} />
-      <div className="h-32 md:h-48"></div>
+  // return (
+  //   <div className=" flex flex-col items-center justify-center ">
+  //     <Header fix={fix} />
+  //     <div className="h-32 md:h-48"></div>
 
-      <Divider text="What HSCC will do this year:" />
-      <div className="md:my-[100px] my-[50px] flex flex-col justify-center px-5 md:p-0">
-        <Image className=" rounded-3xl" src={code} width="900px" height={'436px'}></Image>
-        <h1 className=" font-bebas text-white text-[30px] text-center"> Learn to code</h1>
-      </div>
+  //     <Divider text="What HSCC will do this year:" />
+  //     <div className="md:my-[100px] my-[50px] flex flex-col justify-center px-5 md:p-0">
+  //       <Image className=" rounded-3xl" src={code} width="900px" height={'436px'}></Image>
+  //       <h1 className=" font-bebas text-white text-[30px] text-center"> Learn to code</h1>
+  //     </div>
 
-      <div className="md:my-[100px] my-[50px] flex flex-col justify-center px-5 md:p-0">
-        <Image className=" rounded-3xl" src={schoolpic2} width="900px" height={'436px'}></Image>
-        <h1 className=" font-bebas text-white text-[30px] text-center"> make hobby school's website</h1>
-      </div>
+  //     <div className="md:my-[100px] my-[50px] flex flex-col justify-center px-5 md:p-0">
+  //       <Image className=" rounded-3xl" src={schoolpic2} width="900px" height={'436px'}></Image>
+  //       <h1 className=" font-bebas text-white text-[30px] text-center"> make hobby school's website</h1>
+  //     </div>
 
-      <Divider text="Schedule / location" />
-      <div className="flex flex-col md:my-[100px] my-[50px] items-center">
-        <div>
-          <div className=" flex flex-row justify-start md:my-2">
-            <h1 className=" font-bebas text-[#EDAE49] text-[25px] md:text-[30px] w-24  "> time:</h1>
-            <h1 className=" font-bebas text-white text-[20px] md:text-[25px] w-32 md:w-40 "> EVERY TUEsDAY</h1>
-            <h1 className=" font-sans text-white text-[18px] md:text-[20px]  "> 3:15 - 4:30</h1>
-          </div>
-          <div className=" flex flex-row justify-start md:my-2">
-            <h1 className=" font-bebas text-[#EDAE49] text-[25px] md:text-[30px] w-24  "> place:</h1>
-            <h1 className=" font-bebas text-white text-[20px] md:text-[25px] w-32 md:w-40 "> Room</h1>
-            <h1 className=" font-sans text-white text-[18px] md:text-[20px]  "> 312</h1>
-          </div>
-        </div>
-      </div>
+  //     <Divider text="Schedule / location" />
+  //     <div className="flex flex-col md:my-[100px] my-[50px] items-center">
+  //       <div>
+  //         <div className=" flex flex-row justify-start md:my-2">
+  //           <h1 className=" font-bebas text-[#EDAE49] text-[25px] md:text-[30px] w-24  "> time:</h1>
+  //           <h1 className=" font-bebas text-white text-[20px] md:text-[25px] w-32 md:w-40 "> EVERY TUEsDAY</h1>
+  //           <h1 className=" font-sans text-white text-[18px] md:text-[20px]  "> 3:15 - 4:30</h1>
+  //         </div>
+  //         <div className=" flex flex-row justify-start md:my-2">
+  //           <h1 className=" font-bebas text-[#EDAE49] text-[25px] md:text-[30px] w-24  "> place:</h1>
+  //           <h1 className=" font-bebas text-white text-[20px] md:text-[25px] w-32 md:w-40 "> Room</h1>
+  //           <h1 className=" font-sans text-white text-[18px] md:text-[20px]  "> 312</h1>
+  //         </div>
+  //       </div>
+  //     </div>
 
-      <SecondPart />
-    </div>
-  )
+  //     <SecondPart />
+  //   </div>
+  // )
+
+  return <Error/>
 }
